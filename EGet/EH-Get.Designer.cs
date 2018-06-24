5bc51f559ecc477e7c64ff73022196faed05c2f0
吴优 <wuyou@adinnet.cn>
2018-06-24 14:57:22 +0800

    Initial commit

---
+++ EGet/EH-Get.Designer.cs
@@ -0,0 +1,146 @@
+﻿namespace EGet
+{
+    partial class EH_Get
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(EH_Get));
+            this.lblUrl = new System.Windows.Forms.Label();
+            this.txtTargetUrl = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnDownload = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtSaveDest = new System.Windows.Forms.TextBox();
+            this.brnSelect = new System.Windows.Forms.Button();
+            this.lblTip = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblUrl
+            // 
+            this.lblUrl.AutoSize = true;
+            this.lblUrl.Location = new System.Drawing.Point(30, 73);
+            this.lblUrl.Name = "lblUrl";
+            this.lblUrl.Size = new System.Drawing.Size(65, 12);
+            this.lblUrl.TabIndex = 0;
+            this.lblUrl.Text = "网页地址：";
+            // 
+            // txtTargetUrl
+            // 
+            this.txtTargetUrl.Location = new System.Drawing.Point(100, 70);
+            this.txtTargetUrl.Name = "txtTargetUrl";
+            this.txtTargetUrl.Size = new System.Drawing.Size(363, 21);
+            this.txtTargetUrl.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.Color.Red;
+            this.label1.Location = new System.Drawing.Point(98, 103);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(371, 12);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "注：先选择存放地址，然后输入你要爬取的本子网页地址（仅限E站）";
+            // 
+            // btnDownload
+            // 
+            this.btnDownload.Location = new System.Drawing.Point(494, 70);
+            this.btnDownload.Name = "btnDownload";
+            this.btnDownload.Size = new System.Drawing.Size(84, 23);
+            this.btnDownload.TabIndex = 3;
+            this.btnDownload.Text = "一键下载";
+            this.btnDownload.UseVisualStyleBackColor = true;
+            this.btnDownload.Click += new System.EventHandler(this.btnDownload_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(30, 31);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(65, 12);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "存放路径：";
+            // 
+            // txtSaveDest
+            // 
+            this.txtSaveDest.Enabled = false;
+            this.txtSaveDest.Location = new System.Drawing.Point(100, 28);
+            this.txtSaveDest.Name = "txtSaveDest";
+            this.txtSaveDest.Size = new System.Drawing.Size(363, 21);
+            this.txtSaveDest.TabIndex = 5;
+            // 
+            // brnSelect
+            // 
+            this.brnSelect.Location = new System.Drawing.Point(494, 26);
+            this.brnSelect.Name = "brnSelect";
+            this.brnSelect.Size = new System.Drawing.Size(84, 23);
+            this.brnSelect.TabIndex = 6;
+            this.brnSelect.Text = "选择文件夹";
+            this.brnSelect.UseVisualStyleBackColor = true;
+            this.brnSelect.Click += new System.EventHandler(this.brnSelect_Click);
+            // 
+            // lblTip
+            // 
+            this.lblTip.AutoSize = true;
+            this.lblTip.ForeColor = System.Drawing.Color.Red;
+            this.lblTip.Location = new System.Drawing.Point(100, 135);
+            this.lblTip.Name = "lblTip";
+            this.lblTip.Size = new System.Drawing.Size(0, 12);
+            this.lblTip.TabIndex = 7;
+            // 
+            // EH_Get
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(640, 177);
+            this.Controls.Add(this.lblTip);
+            this.Controls.Add(this.brnSelect);
+            this.Controls.Add(this.txtSaveDest);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnDownload);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtTargetUrl);
+            this.Controls.Add(this.lblUrl);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "EH_Get";
+            this.Text = "E站本子下载器 —— by EricaW";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblUrl;
+        private System.Windows.Forms.TextBox txtTargetUrl;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnDownload;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtSaveDest;
+        private System.Windows.Forms.Button brnSelect;
+        private System.Windows.Forms.Label lblTip;
+    }
+}
---
+++ EGet/EH-Get.cs
@@ -0,0 +1,182 @@
+﻿using Ivony.Html;
+using Ivony.Html.Parser;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EGet
+{
+    public partial class EH_Get : Form
+    {
+        public EH_Get()
+        {
+            InitializeComponent();
+            //取消跨线程检查(重要)
+            Control.CheckForIllegalCrossThreadCalls = false;
+        }
+
+        /// <summary>
+        /// 存放本子的路径
+        /// </summary>
+        public string SaveFolder { get; set; }
+        private void btnDownload_Click(object sender, EventArgs e)
+        {
+            //root url https://e-hentai.org 
+            this.lblTip.Text = string.Empty;
+            string targetUrl = this.txtTargetUrl.Text; //本子根目录
+            string targetText = this.txtTargetUrl.Text;//获取本子的标题
+            string savePath = this.txtSaveDest.Text;
+            if (string.IsNullOrEmpty(savePath)) {
+                MessageBox.Show("请先选择本子的存放目录！", "提示", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                return;
+            }
+            if (string.IsNullOrEmpty(targetUrl)) {
+                MessageBox.Show("请先选择下载地址！", "提示", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                return;
+            }
+            try
+            {
+                btnDownload.Text = "正在下载";
+                btnDownload.Enabled = false;
+
+                var targetObj = new JumonyParser().LoadDocument(targetUrl).Find("#gj");
+                if (targetObj != null && targetObj.FirstOrDefault() != null)
+                {
+                    targetText = targetObj.FirstOrDefault().InnerText();
+                }
+                RunAsync(() =>
+                {
+                    GetAllLinkPage(targetUrl, targetText);
+                });
+                
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("发生了我也不想的错误.....:"+ex.ToString(), "提示", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        /// <summary>
+        /// 获取目标页面要抓取的详细图片地址
+        /// </summary>
+        /// <param name="targetUrl">本子的目录地址</param>
+        /// <param name="targetText">本子名称</param>
+        public void GetAllLinkPage(string targetUrl,string targetText)
+        {
+            int countNum = 0;//页面计数
+            List<string> urlToDownLoad = new List<string>();//抓取的地址集合
+            urlToDownLoad.Add(targetUrl);
+            Dictionary<string, string> dicUrls = new Dictionary<string, string>();
+            var target = new JumonyParser().LoadDocument(targetUrl).Find(".gdtm a");
+            foreach (var eachUrl in target)
+            {
+                countNum++;
+                if (eachUrl == null || eachUrl.Attribute("href") == null) continue;
+                dicUrls.Add(countNum.ToString(), eachUrl.Attribute("href").AttributeValue.ToString());
+            }
+            //可能有多页
+            var pager = new JumonyParser().LoadDocument(targetUrl).Find(".gtb a");
+            foreach (var p in pager)
+            {
+                if (p == null || p.Attribute("href") == null) continue;
+                if (p.Attribute("href").AttributeValue.ToString().ToLower().IndexOf("p=") < 0) continue;//说明只有一页
+                string newPage = p.Attribute("href").AttributeValue.ToString();
+                if (urlToDownLoad.Contains(newPage)) { continue; }
+                urlToDownLoad.Add(newPage);
+                var nextTarget = new JumonyParser().LoadDocument(newPage).Find(".gdtm a");
+                foreach (var eachUrl in nextTarget)
+                {
+                    countNum++;
+                    if (eachUrl == null || eachUrl.Attribute("href") == null) continue;
+                    dicUrls.Add(countNum.ToString(), eachUrl.Attribute("href").AttributeValue.ToString());
+                }
+            }
+            DownLoadImg(dicUrls, targetText);
+        }
+
+        /// <summary>
+        /// 开始下载本子
+        /// </summary>
+        /// <param name="dicUrls">下载目标</param>
+        /// <param name="targetText">下载目录</param>
+        public void DownLoadImg(Dictionary<string,string> dicUrls,string targetText) 
+        {
+            //制定本子的存放目录
+            string path = Path.Combine(this.SaveFolder, targetText);
+            if (!Directory.Exists(path)) {
+                Directory.CreateDirectory(path);
+            }
+            WebClient webClient = new WebClient();
+            //设置请求头
+            webClient.Headers["User-Agent"] = @"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36";
+            //开始遍历下载
+            if (dicUrls.Count > 0)
+            {
+                int indexPage=0;
+                foreach (var eachPage in dicUrls)
+                {
+                    indexPage++;
+                    lblTip.Text = string.Format("正在下载第{0}页，请耐心等待.....", indexPage);
+                    try
+                    {
+                        var targetPage = new JumonyParser().LoadDocument(eachPage.Value).Find("#i3 img");
+                        if (targetPage.FirstOrDefault() == null) continue;
+                        string imgToDownload = targetPage.FirstOrDefault().Attribute("src").AttributeValue.ToString();
+                        string destFileName = Path.Combine(path, eachPage.Key + ".jpg");
+                        webClient.DownloadFile(imgToDownload, destFileName);
+                    }
+                    catch (Exception ex)
+                    {
+                        continue;
+                    }
+                }
+                btnDownload.Text = "点击下载";
+                btnDownload.Enabled = true;
+                lblTip.Text = string.Empty;
+                MessageBox.Show("下载完成，快去看看吧hentai","提示", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                //下载完成，提示
+                System.Diagnostics.Process.Start("Explorer.exe", path);
+            }
+        }
+
+        /// <summary>
+        /// 选择存放的文件夹
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void brnSelect_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog path = new FolderBrowserDialog();
+            path.ShowDialog();
+            this.SaveFolder = path.SelectedPath;
+            this.txtSaveDest.Text = path.SelectedPath;
+        }
+
+        // 异步线程，防止UI假死
+        public static void RunAsync(Action action)
+        {
+            ((Action)(delegate ()
+            {
+                action.Invoke();
+            })).BeginInvoke(null, null);
+        }
+
+        public void RunInMainthread(Action action)
+        {
+            this.BeginInvoke((Action)(delegate ()
+            {
+                action.Invoke();
+            }));
+        }
+        
+    }
+}
